efacde03fcf404ef8818b29c6c08ee4f904235fe
Adam Schubert <adam.schubert@sg1-game.net>
2022-06-28 17:20:54 +0200

    feat(Debian): Package config

---
+++ debian/qt-kiosk.default.ex
@@ -0,0 +1,10 @@
+# Defaults for tux-control-frontend initscript
+# sourced by /etc/init.d/tux-control-frontend
+# installed at /etc/default/tux-control-frontend by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
