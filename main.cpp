e2babee82e8b904c3cbc56d7352b579771f14946
Adam Schubert <adam.schubert@sg1-game.net>
2022-07-14 17:12:05 +0200

    feat(Version): Added .version.yml and fixes

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
     QCoreApplication::setOrganizationName("Adam Schubert");
     QCoreApplication::setApplicationName("qiosk");
-    QCoreApplication::setApplicationVersion("1.0");
+    QCoreApplication::setApplicationVersion("1.0.0");
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QApplication a(argc, argv);
