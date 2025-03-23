3e91252c468ed5189f27b6bf34d0da00ef14ebf1
Adam Schubert <adam.schubert@sg1-game.net>
2025-03-23 18:48:07 +0100

    New version 1.12.2

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 
     QCoreApplication::setOrganizationName("Salamek");
     QCoreApplication::setApplicationName("qiosk");
-    QCoreApplication::setApplicationVersion("1.12.1");
+    QCoreApplication::setApplicationVersion("1.12.2");
 #if QT_VERSION <= QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
